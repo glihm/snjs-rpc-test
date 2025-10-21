e089a1cd88926ec5ffb4227489c9c4b1b2d21835
glihm <dev@glihm.net>
2025-10-21 15:09:41 -0600

    fix: remove API key and add new RPC method

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,63 @@
 <script>
 	import { RpcProvider } from 'starknet';
 
-    const cggRpcUrl = 'https://api.cartridge.gg/x/starknet/sepolia';
-    const corsAnywhereUrl = `https://cors-anywhere.com/${cggRpcUrl}`;
+	const cggRpcUrl = 'https://api.cartridge.gg/x/starknet/sepolia';
+	//const corsAnywhereUrl = `https://cors-anywhere.com/${cggRpcUrl}`;
 
 	const cggProvider = new RpcProvider({
-		nodeUrl: corsAnywhereUrl,
-        headers: {
-            'Authorization': 'Bearer ' + import.meta.env.VITE_CGG_API_KEY,
-        }
+		nodeUrl: cggRpcUrl
 	});
 
-    let loading = $state(false);
+	let loading = $state(false);
 	let blockNumber = $state(0);
+	let rpcSpec = $state('');
 
-    const getBlockNumber = async () => {
+	const getBlockNumber = async () => {
+		loading = true;
+		try {
+			blockNumber = await cggProvider.getBlockNumber();
+		} catch (error) {
+			console.error(error);
+		} finally {
+			loading = false;
+		}
+	};
+
+    const getRpcSpec = async () => {
         loading = true;
         try {
-            blockNumber = await cggProvider.getBlockNumber();
+            rpcSpec = await cggProvider.getSpecVersion();
         } catch (error) {
             console.error(error);
         } finally {
             loading = false;
         }
-    }
+    };
 </script>
 
 <h1>Starknet Sepolia RPC Provider test</h1>
 
-<button onclick={getBlockNumber}>
-    {#if loading}
-        <span>Loading...</span>
-    {:else}
-        Get Block Number
-    {/if}
-</button>
+<div>
+	<button onclick={getBlockNumber}>
+		{#if loading}
+			<span>Loading...</span>
+		{:else}
+			Get Block Number
+		{/if}
+	</button>
 
-<p>Block Number: {blockNumber}</p>
+	<p>Block Number: {blockNumber}</p>
+</div>
+
+
+<div>
+	<button onclick={getRpcSpec}>
+		{#if loading}
+			<span>Loading...</span>
+		{:else}
+			Get RPC Spec
+		{/if}
+	</button>
+
+	<p>RPC Spec: {rpcSpec}</p>    
+</div>
